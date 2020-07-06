f0f98a72913a2eff58f7725949f94e851c62439b
triacontakai <totally.not.andrei@gmail.com>
2020-07-05 20:39:23 -0400

    Added spinner animation prototype Uses intervals to update state when re-rendering spinner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Spinner x={window.innerWidth/2} y={window.innerHeight/2} radius={300} items={["yep", "cock", "poggers"]} />
+        <Spinner x={window.innerWidth/2} y={window.innerHeight/2} radius={400} items={["yep", "cock", "poggers"]} />
       </Layer>
     </Stage>
   );
--- src/Spinner.js
+++ src/Spinner.js
@@ -3,18 +3,43 @@ import Konva from 'konva';
 import { Wedge } from 'react-konva';
 
 class Spinner extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            spinAngle: 200,
+        };
+        this.colors = this.props.items.map(() => Konva.Util.getRandomColor());
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            spinAngle: this.state.spinAngle + 3
+        });
+    }
+
     render() {
         const sliceAngle = 360 / this.props.items.length;
     
         let slices = [];
-        let currentRotation = 0;
+        let currentRotation = this.state.spinAngle;
         for (let i = 0; i < this.props.items.length; i++) {
             slices.push(<Wedge
                 x={this.props.x}
                 y={this.props.y}
                 radius={this.props.radius}
                 angle={sliceAngle}
-                fill={Konva.Util.getRandomColor()}
+                fill={this.colors[i]}
                 rotation={currentRotation}
             />);
 
